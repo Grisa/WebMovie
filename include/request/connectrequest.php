154b42d563b975feb5643e6145dd164ed8baf535
Ramiro Grisa <ramirogrisa@gmail.com>
2019-06-01 17:37:27 -0300

    Continuacao do back end

--- include/request/connectrequest.php
+++ include/request/connectrequest.php
@@ -1,8 +1,26 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-ob_start();
 
+require_once "../dbapi/controller/class.controllerapi.inc";
 
+//$jsonFormat = new DbApi\Controller\JsonFormatter();
+$controller = new \DbApi\Controller\ControllerApi();
+
+if (!empty($_REQUEST['restType'])) {
+    switch ($_REQUEST['restType']) {
+        case 'createUser':
+            $response = $controller->createNewUser($_REQUEST['username'], $_REQUEST['password']);
+        break;
+        case 'validUser':
+
+        break;
+        case 'likeCount':
+
+        break;
+    }
+    error_log($response);
+    echo $response;
+}
 
 ?>
